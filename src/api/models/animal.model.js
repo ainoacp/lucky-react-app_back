f8823e4fb237d869d0765734317a898d8574c141
ainoacp <118761900+ainoacp@users.noreply.github.com>
2023-02-16 13:47:48 +0000

    breed

--- src/api/models/animal.model.js
+++ src/api/models/animal.model.js
@@ -4,20 +4,84 @@ const mongoose = require('mongoose');
 // 1 - SCHEMA
 const animalSchema = mongoose.Schema(
     {
-    "especie": {type:'String', required:true},
-    "fecha de nacimiento":  {type:'Date', required:true},
-    "sexo": {type:'String', required:true},
-    "tamaño": {type:'string', required:true},
-    "peso": {type:'Number', required:true},
-    "vacunado": {type:'Boolean', required:true},
-    "desparasitado": {type:'Boolean', required:true},
-    "sano": {type:'Boolean', required:true},
-    "esterilizado": {type:'Boolean', required:true},
-    "identificado": {type:'Boolean', required:true},
-    "microchip": {type:'Boolean', required:true},
-    "nombre": {type:'String', required:true},
-    "ubicacion": {type:'String', required:true},
-    "ciudad": {type:'String', required:true},
+        "especie": {
+            type:'String', 
+            required:true, 
+            enum:[ 
+                "Perro", 
+                "Gato", 
+                "Conejo", 
+                "Cobaya", 
+                "Pequeño mamífero", 
+                "Hurón", 
+                "Pez", 
+                "Reptil", 
+                "Anfibio", 
+                "Arácnido o insecto", 
+                "Ave"
+            ]
+        },
+        "fecha de nacimiento": {
+            type:'Date', 
+            required:true
+        },
+        "sexo": {
+            type:'String', 
+            required:true,
+            enum: [
+                "Hembra",
+                "Macho"
+            ]
+        },
+        "tamaño": {
+            type:'string', 
+            required:true,
+            enum: [
+                "Pequeño",
+                "Mediano",
+                "Grande"
+            ]
+        },
+        "peso": {
+            type:'Number', 
+            required:true
+        },
+        "vacunado": {
+            type:'Boolean', 
+            required:true
+        },
+        "desparasitado": {
+            type:'Boolean', 
+            required:true
+        },
+        "sano": {
+            type:'Boolean', 
+            required:true
+        },
+        "esterilizado": {
+            type:'Boolean', 
+            required:true
+        },
+        "identificado": {
+            type:'Boolean', 
+            required:true
+        },
+        "microchip": {
+            type:'Boolean', 
+            required:true
+        },
+        "nombre": {
+            type:'String', 
+            required:true
+        },
+        "ubicacion": {
+            type:'String', 
+            required:true
+        },
+        "ciudad": {
+            type:'String', 
+            required:true
+        },
     },
     {timestamps:true}
 );
