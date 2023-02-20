fd5547c45f8515a4860c73fd084854596fbec646
ainoacp <118761900+ainoacp@users.noreply.github.com>
2023-02-20 14:14:12 +0000

    fixed checksession

--- src/api/routes/user.routes.js
+++ src/api/routes/user.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const {register, login, getUsers, getUserById} = require('../controllers/user.controller');
+const {register, login, checkSession} = require('../controllers/user.controller');
+const {isAuth} = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.get ('/', getUsers);
-router.get('/:id', getUserById)
-// router.post('/') checksession
+// router.get ('/', getUsers);
+// router.get('/:id', getUserById)
+router.post('/checksession',[isAuth], checkSession)
 
 module.exports = router;
