b5bb6d84129a4b1da319a5b8649564a68b9c3bf8
ainoacp <118761900+ainoacp@users.noreply.github.com>
2023-02-21 09:12:53 +0000

    act add favs

--- src/api/routes/user.routes.js
+++ src/api/routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {register, login, checkSession} = require('../controllers/user.controller');
+const {register, login, checkSession, postFav} = require('../controllers/user.controller');
 const {isAuth} = require('../middleware/auth');
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.post('/register', register);
 router.post('/login', login);
 // router.get ('/', getUsers);
 // router.get('/:id', getUserById)
-router.post('/checksession',[isAuth], checkSession)
+router.post('/checksession',[isAuth], checkSession);
+router.post('/addfav', postFav)
 
 module.exports = router;
